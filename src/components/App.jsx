de6ab0446fd4b872aa16ef94ba9ff1b7fe18ff0a
AndriiMurchk <andr.murchik@gmail.com>
2023-02-19 22:30:28 +0200

    Додані усі json файлт

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import { Profile } from "./Profile/Profile";
-
-
+import user from '../user.json'
 
 export const App = () => {
   return <>
-  <Profile />
+  <Profile user = {user} />
   </>;
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,3 +1,19 @@
-export const Profile = () => {
-  return <div>Profile</div>;
-}
+import PropTypes from 'prop-types'
+
+export const Profile = ({
+  user:{username,tag, location, avatar, stats}}) => {
+  return (
+    <div>
+    <img src={avatar} alt={username}  width="240" />
+    <h2>{username}</h2>
+    <p>@{tag}</p>
+    <p>{location}</p>
+    <ul>
+    <li>Folowers{stats.followers}</li>
+    <li>Views{stats.views}</li>
+    <li>Likes{stats.likes}</li>
+  </ul>
+  </div>
+  );
+};
+
